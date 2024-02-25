a77c2c0a0ca67b6a91fd8696a90740bfbae32e56
Q <vfishv@gmail.com>
2024-02-25 20:27:21 +0800

    androidx.media3:media3-exoplayer:1.2.1"

--- rxmusicplayer/src/main/java/com/orfium/rx/musicplayer/playback/PlaybackImpl.kt
+++ rxmusicplayer/src/main/java/com/orfium/rx/musicplayer/playback/PlaybackImpl.kt
@@ -2,30 +2,20 @@ package com.orfium.rx.musicplayer.playback
 
 import android.content.Context
 import android.media.AudioManager
-import android.net.Uri
 import android.net.wifi.WifiManager
-import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.audio.AudioAttributes
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
 import com.orfium.rx.musicplayer.R
 
 internal class PlaybackImpl(
     context: Context,
     audioManager: AudioManager,
     wifiLock: WifiManager.WifiLock
-) : BasePlayback(context, audioManager, wifiLock), Player.EventListener {
+) : BasePlayback(context, audioManager, wifiLock), Player.Listener {
 
-    private var player: SimpleExoPlayer = ExoPlayerFactory.newSimpleInstance(
-        context, DefaultRenderersFactory(context), DefaultTrackSelector(), DefaultLoadControl()
-    ).apply {
-        audioAttributes = AudioAttributes.Builder()
-            .setContentType(C.CONTENT_TYPE_MUSIC)
-            .build()
-    }
+    private var player = ExoPlayer.Builder(context).build()
 
     override val isPlaying: Boolean
         get() = player.playWhenReady
@@ -58,7 +48,14 @@ internal class PlaybackImpl(
         player.seekTo(position)
     }
 
-    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+    var playWhenReady = false
+    override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+        this.playWhenReady = playWhenReady
+        super.onPlayWhenReadyChanged(playWhenReady, reason)
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val playWhenReady = player.playWhenReady
         when (playbackState) {
             Player.STATE_IDLE -> playbackCallback?.onIdle()
             Player.STATE_READY -> if (playWhenReady) playbackCallback?.onPlay() else playbackCallback?.onPause()
@@ -67,19 +64,44 @@ internal class PlaybackImpl(
         }
     }
 
-    override fun onPlayerError(error: ExoPlaybackException?) {
+    /*
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        when (playbackState) {
+            Player.STATE_IDLE -> playbackCallback?.onIdle()
+            Player.STATE_READY -> if (playWhenReady) playbackCallback?.onPlay() else playbackCallback?.onPause()
+            Player.STATE_BUFFERING -> if(playWhenReady) playbackCallback?.onBuffer() else playbackCallback?.onPause()
+            Player.STATE_ENDED -> playbackCallback?.onCompletion()
+        }
+    }
+    */
+
+    override fun onPlayerError(error: PlaybackException) {
         playbackCallback?.onError()
     }
 
     private fun play() {
+        /*
         val dataSourceFactory = DefaultDataSourceFactory(
             context, Util.getUserAgent(context, context.getString(R.string.app_name)), null
         )
         val mediaSource = ExtractorMediaSource.Factory(dataSourceFactory)
             .setExtractorsFactory(DefaultExtractorsFactory())
             .createMediaSource(Uri.parse(currentMedia?.streamUrl))
+        */
+
+        /*
+        val mediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory())
+            .createMediaSource(MediaItem.fromUri(Uri.parse(currentMedia?.streamUrl)))
+            */
+        //val mediaItem = MediaItem.fromUri(Uri.parse(currentMedia?.streamUrl))
+
+        val mediaItem = MediaItem.Builder()
+            .setUri(currentMedia?.streamUrl)
+            .build()
         player.addListener(this)
-        player.prepare(mediaSource)
+        player.setMediaItem(mediaItem)
+        //player.setMediaSource(mediaSource)
+        player.prepare()
         player.playWhenReady = true
     }
 }
